10731a2f0ba64fdb0a2568a999b7e1ebafcf421c
Joel Ting <joelccting@gmail.com>
2026-07-09 17:42:48 +0800

    modified:   cleanup.bat

--- cleanup.bat
+++ cleanup.bat
@@ -2,7 +2,7 @@
 REM Delete all .exe files in current directory and subdirectories
 for /r %%i in (*.exe) do (
     echo Deleting "%%i"
-    del "%%i"
+    powershell -command "Remove-Item '%%i' -Recycle"
 )
 echo Done!
 pause
