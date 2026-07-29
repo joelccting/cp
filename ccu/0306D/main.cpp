f40923f6b35c47c2f4f4e7dda779943136999378
Joel Ting <joelccting@gmail.com>
2026-07-29 21:23:57 +0800

    Modified   ccu/0306D/main.cpp

--- ccu/0306D/main.cpp
+++ ccu/0306D/main.cpp
@@ -1,59 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
+int n, m;
+const int dx[] = {-1, 1, 0, 0};
+const int dy[] = {0, 0, -1, 1};
+vector<string> building;
+void dfs(int x, int y)
+{
+    // Mark current cell as visited by turning it into a wall
+    building[x][y] = '#';
+    for (int i = 0; i < 4; ++i)
+    {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        // Bounds check + check if it's an unvisited floor cell
+        if (nx >= 0 && nx < n && ny >= 0 && ny < m && building[nx][ny] == '.')
+        {
+            dfs(nx, ny);
+        }
+    }
+}
+
 int main()
 {
-    int n, m;
     if (!(cin >> n >> m))
         return 0;
-    // char building[n][m + 1];
-    vector<string> building(n);
-    vector<vector<bool>> visited(n, vector<bool>(m, false));
+
+    building.resize(n);
     for (int i = 0; i < n; ++i)
     {
         cin >> building[i];
     }
 
     int room_count = 0;
-    stack<pair<int, int>> s;
     for (int i = 0; i < n; ++i)
     {
         for (int j = 0; j < m; ++j)
         {
-            if (building[i][j] == '#')
-                continue;
-
-            if (visited[i][j] == false)
+            if (building[i][j] == '.')
             {
-                s.push({i, j});
-                visited[i][j] = true;
+                dfs(i, j);
                 ++room_count;
-                while (!s.empty())
-                {
-                    pair<int, int> p = s.top();
-                    s.pop();
-                    if (p.first + 1 < n && visited[p.first + 1][p.second] == false && building[p.first + 1][p.second] == '.')
-                    {
-                        visited[p.first + 1][p.second] = true;
-                        s.push({p.first + 1, p.second});
-                    }
-
-                    if (p.first - 1 > -1 && visited[p.first - 1][p.second] == false && building[p.first - 1][p.second] == '.')
-                    {
-                        visited[p.first - 1][p.second] = true;
-                        s.push({p.first - 1, p.second});
-                    }
-
-                    if (p.second + 1 < m && visited[p.first][p.second + 1] == false && building[p.first][p.second + 1] == '.')
-                    {
-                        visited[p.first][p.second + 1] = true;
-                        s.push({p.first, p.second + 1});
-                    }
-                    if (p.second - 1 > -1 && visited[p.first][p.second - 1] == false && building[p.first][p.second - 1] == '.')
-                    {
-                        visited[p.first][p.second - 1] = true;
-                        s.push({p.first, p.second - 1});
-                    }
-                }
             }
         }
     }
