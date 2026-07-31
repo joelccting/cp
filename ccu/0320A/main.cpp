0b3b0c3c51c7c03c18bd0925226e9b496783da69
Joel Ting <joelccting@gmail.com>
2026-07-31 21:00:35 +0800

    Modified   ccu/0320A/main.cpp Added      ccu/0320B/main.cpp

--- ccu/0320A/main.cpp
+++ ccu/0320A/main.cpp
@@ -6,6 +6,55 @@ typedef vector<vector<int>> VVI;
 typedef vector<int> VI;
 typedef vector<bool> VB;
 
+#define RECURSION
+
+#if defined(RECURSION)
+
+void dfs(int u, int p, int d, const VVI &adj, VI &dist)
+{
+    dist[u] = d;
+    for (int v : adj[u])
+    {
+        if (v != p)
+        {
+            dfs(v, u, d + 1, adj, dist);
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    if (!(cin >> n))
+        return 0;
+
+    VVI adj(n + 1);
+    for (int a, b, i = 1; i < n; ++i)
+    {
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    VI dist(n + 1, 0);
+
+    // 第一次 DFS：找距離 1 最遠的點 farest
+    dfs(1, 0, 0, adj, dist);
+    int farest = max_element(dist.begin() + 1, dist.end()) - dist.begin();
+
+    // 第二次 DFS：從 farest 出發找最遠距離（即樹直徑）
+    dfs(farest, 0, 0, adj, dist);
+    int diameter = *max_element(dist.begin() + 1, dist.end());
+
+    cout << diameter << "\n";
+
+    return 0;
+}
+
+#else
 void dfs(int start, VVI &adj, VI &dist, VB &vis)
 {
     stack<int> s;
@@ -72,10 +121,11 @@ int main()
         {
             maxDist = distance[i];
         }
-            
     }
 
     cout << maxDist;
 
     return 0;
 }
+
+#endif
---
+++ ccu/0320B/main.cpp
@@ -0,0 +1,59 @@
+// The problem asks for the minimum number of additional edges needed
+// so that every vertex can reach every other vertex.
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<vector<int>> VVI;
+
+int findParent(int i, vector<int> &parent)
+{
+    if (i == parent[i])
+        return i;
+
+    return parent[i] = findParent(parent[i], parent); ////路徑壓縮
+    // return findParent(parent[i], parent); //路徑不壓縮
+}
+
+void merge(int a, int b, vector<int> &parent, vector<int> &size)
+{
+    int pa = findParent(a, parent);
+    int pb = findParent(b, parent);
+    if (pa == pb)
+        return;
+
+    // 啟發式合併
+    if (size[pa] < size[pb])
+        swap(pa, pb);
+    size[pa] += size[pb];
+
+    parent[pb] = pa;
+}
+
+int main()
+{
+    int n, m;
+    if (!(cin >> n >> m))
+        return 0;
+
+    vector<int> size(n + 1, 1);
+    vector<int> parent(n + 1);
+    for (int i = 1; i <= n; ++i)
+    {
+        parent[i] = i;
+    }
+
+    for (int i = 0, a, b; i < m; ++i)
+    {
+        cin >> a >> b;
+        merge(a, b, parent, size);
+    }
+
+    int numOfComponents = 0;
+    for (int i = 1; i <= n; ++i)
+    {
+        if (parent[i] == i)
+            numOfComponents++;
+    }
+
+    cout << numOfComponents - 1;
+    return 0;
+}
